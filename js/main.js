581bdb97152e0ced5ebac28bcfce221be9651626
jourzee <jourzee@gmail.com>
2014-04-14 16:42:19 +0100

    contactpage1

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ window._skel_config = {
         preloadStyleSheets: true,
     resetCSS: true,
     boxModel: 'border',
-    grid: { gutters: 10 },
+    grid: { gutters: 30 },
     breakpoints: {
-        wide: { range: '1200-', containers: 1140, grid: { gutters: 10 } },
+        wide: { range: '1200-', containers: 1140, grid: { gutters: 50 } },
         narrow: { range: '481-1199', containers: 960 },
         mobile: { range: '-480', containers: 'fluid', lockViewport: true, grid: { collapse: true } }
     }

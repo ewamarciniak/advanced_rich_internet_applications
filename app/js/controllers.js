5f89391ceace2ac284899eab94089b3d081d83ff
Thomas Dowling <thomas@thomas-Vostro-2520.(none)>
2014-04-15 18:01:42 +0100

    Removed extraeneous code from partials and controllers

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,104 +1,43 @@
 'use strict';
 /* Controllers */
-//alert('alert first');
+
 angular.module('myApp.controllers', [])
   .controller('MyCtrl1', [function() {
 
-        //alert('hello c1')
+        //Partial One code Goes Here
+
+        //alert('hello from partial One')
 
    }])
     .controller('MyCtrl2', [function() {
 
-        var mymodule = (function () {
-        //Private
+        //Partial Two code Goes Here
 
-        //Public
-            return {
-                $$: function (id) {
-                    'use strict';
-                    if (typeof id != 'undefined') {
-                        return document.getElementById(id);
-                    }
-                    else {
-                        return 'undefined'
-                    }
-                },
-
-                showDivs: function (arg) {
-                    document.getElementById(arg).style.visibility = 'visible';
-                },
-
-                hideDivs: function (arg) {
-                    document.getElementById(arg).style.visibility = 'hidden';
-                }
-
-            }
-        })();
-
-//AddEventListener
-        mymodule.$$('myinput').addEventListener('focus',function () {
-
-            mymodule.showDivs('scorewrapper');
-            return false;
-        }, false);
-
-        mymodule.$$('myinput').addEventListener('blur',function () {
-
-            mymodule.hideDivs('scorewrapper');
-            return false;
-        }, false);
+        //alert('hello from partial Two')
 
     }])
     .controller('MyCtrl3', [function() {
+        //Partial Three code Goes Here
+
+        //alert('hello from partial Three')
 
     }])
     .controller('MyCtrl4', [function() {
 
+        //Partial Four code Goes Here
+
+        //alert('hello from partial Four')
+
+
+        //Sample code.  May be deleted
        document.getElementById('bonnie2').innerHTML = "Added using Inner HTML";
        document.getElementById('bonnie3').style.visibility = 'visible';
-    }]).controller('MyCtrl5', function($scope) {
 
-        $scope.markup = function(arg) {
+    }]).controller('MyCtrl5', function() {
 
-            return arg + 100 * arg};
+        //Partial five code Goes Here
 
-            $scope.services = [
-                {
-                    name: "Web Development",
-                    price: 300,
-                    active: true
-                },
-                {
-                    name: "Design",
-                    price: 400,
-                    active: false
-                },
-                {
-                    name: "Integration",
-                    price: 300,
-                    active: false
-                },
-                {
-                    name: "Training",
-                    price: 220,
-                    active: false
-                }
-            ];
-
-            $scope.toggleActive = function(s) {
-                s.active = !s.active
-            };
-
-            $scope.total = function() {
-                var total = 0;
-
-                angular.forEach($scope.services, function(s) {
-                    if (s.active) {
-                        total+= s.price;
-                    }
-                });
-                return total;
-            } ;
+        alert('hello from partial Five')
 });
 
 

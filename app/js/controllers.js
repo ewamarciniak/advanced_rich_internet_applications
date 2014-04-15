b3ea4e5dbe0096174ab7ebb4e91b4975611428f3
Thomas Dowling <thomas@thomas-Vostro-2520.(none)>
2014-04-15 19:22:20 +0100

    added prelim Gym location to partial 3

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,6 +1,5 @@
 'use strict';
 /* Controllers */
-
 angular.module('myApp.controllers', [])
   .controller('MyCtrl1', [function() {
 
@@ -21,6 +20,55 @@ angular.module('myApp.controllers', [])
 
         //alert('hello from partial Three')
 
+        function FindLocation() {
+            //code by John Fitzpatrick
+            // HDIp COM SC
+            geocoder = new google.maps.Geocoder();
+            InitializeMap();
+
+            var address = document.getElementById("addressinput").value;
+            var gymlocations = new Array();
+            gymlocations[0] = "Cork Street Dublin";
+            gymlocations[1] = "Quarry Road, Cabra, Dublin";
+            gymlocations[2] = "Bishopstown Road, Cork";
+
+            geocoder.geocode({ 'address': address }, function (results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    map.setCenter(results[0].geometry.location);
+                    var marker = new google.maps.Marker({
+                        map: map,
+                        position: results[0].geometry.location
+                    });
+
+
+                    if (address == gymlocations[0]) {
+                        document.getElementById('l1').innerHTML = gymlocations[0]+ 'Tel: 01-2323456';
+
+                    }
+                    if (address == gymlocations[1]) {
+                        document.getElementById('l1').innerHTML = gymlocations[1] + ' Tel: 01 45443321';
+
+                    }
+                    if (address == gymlocations[2]) {
+                        document.getElementById('l1').innerHTML = gymlocations[2] + ' Tel: 01 7675432';
+
+                    }
+                }
+                else {
+                    alert("Not successful for the following reason: " + status);
+                }
+            });
+        }
+
+        FindLocation();
+
+       document.getElementById('Button1').addEventListener('click',function () {
+
+            FindLocation();
+            return false;
+       }, false);
+
+
     }])
     .controller('MyCtrl4', [function() {
 
@@ -37,7 +85,7 @@ angular.module('myApp.controllers', [])
 
         //Partial five code Goes Here
 
-        alert('hello from partial Five')
+        //alert('hello from partial Five')
 });
 
 
